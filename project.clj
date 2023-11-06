ee220b2b59c05828b1353abcb1aac3c70e51d648
Facundo Mastricchio <facundomastricchio@gmail.com>
2023-11-06 11:24:13 -0300

    Minor changes

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject interprete-racket "0.1.0-SNAPSHOT"
   :description "Interprete de Racket"
-  :url "http://example.com/FIXME"
+  :url "http://github.com/FacuMastri/racket-interpreter"
   :dependencies [[org.clojure/clojure "1.10.0"]]
-  :repl-options {:init-ns interprete-racket.core})
+  :repl-options {:init-ns interprete-racket.core}
+  :main interprete-racket.core)
